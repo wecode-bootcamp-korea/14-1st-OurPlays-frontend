503f6409aa0d8c632b18fc4624c0a78d39c5c3a5
jooney <jooneybadger@gmail.com>
2020-11-16 20:45:42 +0900

    초기 세팅 폴더 경로 수정

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Footer from '../src/Components/Footer/Footer';
 import Nav from '../src/Components/Nav/Nav';
-import Main from '../src/Pages/donghakim/Main/Main';
-import SignUp from '../src/Pages/donghakim/SignUp/SignUp';
-import Login from '../src/Pages/youngjoonpark/Login/Login';
-import ProductList from '../src/Pages/youngjoonpark/ProductList/ProductList';
-import ProductDetail from '../src/Pages/donghakim/ProductDetail/ProductDetail';
+import Main from '../src/Pages/Main/Main';
+import SignUp from '../src/Pages/SignUp/SignUp';
+import Login from '../src/Pages/Login/Login';
+import ProductList from '../src/Pages/ProductList/ProductList';
+import ProductDetail from '../src/Pages/ProductDetail/ProductDetail';
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 
@@ -18,9 +18,9 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path='/Footer' component={Footer} />
           <Route exact path='/Nav' component={Nav} />
-          <Route exact path='/' component={Main} />
+          <Route exact path='/Main' component={Main} />
           <Route exact path='/SignUp' component={SignUp} />
-          <Route exact path='/Login' component={Login} />
+          <Route exact path='/' component={Login} />
           <Route exact path='/ProductDetail' component={ProductDetail} />
           <Route exact path='/ProductList' component={ProductList} />
         </Switch>
@@ -28,6 +28,5 @@ class Routes extends React.Component {
     );
   }
 }
-git branch feature/기능이름
 
 export default Routes;
