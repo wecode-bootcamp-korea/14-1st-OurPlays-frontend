ea115ec226d1f4c7443a1024086de0c5b771825b
jooney <jooneybadger@gmail.com>
2020-11-17 22:14:31 +0900

    Nav바 페이지 구현중

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 class Nav extends Component {
   render() {
     return (
       <div className='Nav'>
-        <nav>
+        <section className='nav-background'>
           <section className='main-nav'>
-            <div className='logo-icon'>hourplace</div>
+            <div className='logo-icon'>
+              <img src='../images/youngjoonpark/hourplace-logo.svg' alt='logo-img' />
+            </div>
             <div className='search-input'>
-              <input type='text' />
+              <input type='text' placeholder='촬영의 모든 장소' />
+              <img
+                className='camera-icon'
+                src='../images/youngjoonpark/camera-icon.svg'
+                alt='camera-icon'
+              />
+              <img
+                className='search-icon'
+                src='../images/youngjoonpark/search-icon.png'
+                alt='search-icon'
+              />
             </div>
             <div className='nav-icons'>
-              <a href='a'>aaaaa</a>
-              <a href='a'>bbbbb</a>
-              <a href='a'>ccccc</a>
-              <a href='a'>ddddd</a>
-              <a href='a'>eeeee</a>
+              <Link className='nav-icon' to=''>
+                아워플레이스 소개
+              </Link>
+              <Link className='nav-icon' to=''>
+                촬영장소 등록하기
+              </Link>
+              <Link className='nav-icon' to=''>
+                이용방법
+              </Link>
+              <Link className='nav-icon' to=''>
+                회원가입
+              </Link>
+              <Link className='nav-icon' to=''>
+                로그인
+              </Link>
             </div>
           </section>
-          <section className='side-nav'>
-            <span className='home'>home</span>
+        </section>
+        <section className='side-nav'>
+          <div className='direction'>
+            <span className='home'>Home</span>
+            <img src='../images/youngjoonpark/arrow-img.png' alt='' />
             <span className='route'>route</span>
-          </section>
-        </nav>
+          </div>
+        </section>
       </div>
     );
   }
