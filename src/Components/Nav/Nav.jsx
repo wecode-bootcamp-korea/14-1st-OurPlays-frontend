1abadd182756dbec785429624d6f693067eb6859
jooney <jooneybadger@gmail.com>
2020-11-17 17:47:57 +0900

    불필요한 파일 수정

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,29 +3,7 @@ import './Nav.scss';
 
 class Nav extends Component {
   render() {
-    return (
-      <div className='Nav'>
-        <nav>
-          <section className='main-nav'>
-            {/* <div className='logo-icon'>hourplace</div>
-            <div className='search-input'>
-              <input type='text' />
-            </div> */}
-            <div className='nav-icons'>
-              {/* <a href='a'>aaaaa</a>
-              <a href='a'>bbbbb</a>
-              <a href='a'>ccccc</a>
-              <a href='a'>ddddd</a>
-              <a href='a'>eeeee</a> */}
-            </div>
-          </section>
-          <section className='side-nav'>
-            {/* <span className='home'>home</span>
-            <span className='route'>route</span> */}
-          </section>
-        </nav>
-      </div>
-    );
+    return <div className='Nav'></div>;
   }
 }
 
