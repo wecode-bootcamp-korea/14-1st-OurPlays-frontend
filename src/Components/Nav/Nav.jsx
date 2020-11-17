90460a6ab596f43d01d818ae6b0ca3798650f62e
jooney <jooneybadger@gmail.com>
2020-11-17 17:42:59 +0900

    네브바 초기 상태

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import './Nav.scss';
 
 class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className='Nav'>
+        <nav>
+          <section className='main-nav'>
+            <div className='logo-icon'>hourplace</div>
+            <div className='search-input'>
+              <input type='text' />
+            </div>
+            <div className='nav-icons'>
+              <a href='a'>aaaaa</a>
+              <a href='a'>bbbbb</a>
+              <a href='a'>ccccc</a>
+              <a href='a'>ddddd</a>
+              <a href='a'>eeeee</a>
+            </div>
+          </section>
+          <section className='side-nav'>
+            <span className='home'>home</span>
+            <span className='route'>route</span>
+          </section>
+        </nav>
+      </div>
+    );
   }
 }
 
