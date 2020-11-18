ad5892a20b17903d97c62d1d5a4dfa7c771d6c20
jooney <jooneybadger@gmail.com>
2020-11-18 20:11:42 +0900

    카테고리 컴포넌트 구현중

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
+import './Category.scss';
 
 class Category extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    );
+    return <div className='Category'></div>;
   }
 }
 
