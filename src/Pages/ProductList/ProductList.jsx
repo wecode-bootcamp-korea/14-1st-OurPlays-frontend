235c5899b938c975d8e7327a0aed85658144ad6a
jooney <jooneybadger@gmail.com>
2020-11-19 12:27:22 +0900

    ProductList UI 구현 완료

--- src/Pages/ProductList/ProductList.jsx
+++ src/Pages/ProductList/ProductList.jsx
@@ -11,20 +11,47 @@ class ProductList extends Component {
             <div className='title'>옥상</div>
             <div className='filter'>
               <div className='control-buttons'>
-                <span className='control-button'>날짜</span>
-                <span className='control-button'>인원</span>
-                <span className='control-button'>가격</span>
+                <div className='button-box'>
+                  <button className='control-button'>
+                    <span>날짜</span>
+                    <img src='../images/youngjoonpark/control-bar.png' alt='control-bar' />
+                  </button>
+                </div>
+                <div className='button-box'>
+                  <button className='control-button'>
+                    <span>인원</span>
+                    <img src='../images/youngjoonpark/control-bar.png' alt='control-bar' />
+                  </button>
+                </div>
+                <div className='button-box'>
+                  <button className='control-button'>
+                    <span>가격</span>
+                    <img src='../images/youngjoonpark/control-bar.png' alt='control-bar' />
+                  </button>
+                </div>
               </div>
             </div>
           </section>
           <section className='main'>
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
                   <div className='room-list-title'>글제목 </div>
@@ -46,14 +73,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>Large rooftop with panorama city view!</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -72,14 +111,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -98,14 +149,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -124,14 +187,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -150,14 +225,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -176,14 +263,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -202,14 +301,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -228,14 +339,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
@@ -254,14 +377,26 @@ class ProductList extends Component {
 
             <div className='room-lists'>
               <Link className='room-list-link' to=''>
-                <img
-                  className='room-list-img'
-                  src='../images/youngjoonpark/옥상.jpg'
-                  alt='room-img'
-                />
+                <div className='room-list-imgs'>
+                  <img
+                    className='room-list-img'
+                    src='../images/youngjoonpark/옥상.jpg'
+                    alt='room-img'
+                  />
+                  <img
+                    className='bookmark-empty'
+                    src='../images/youngjoonpark/bookmark-empty.png'
+                    alt='북마크'
+                  />
+                  <img
+                    className='bookmark-filled'
+                    src='../images/youngjoonpark/bookmark-filled.png'
+                    alt='북마크'
+                  />
+                </div>
                 <div className='room-list-text'>
                   <div className='room-list-region'>카테고리/지역</div>
-                  <div className='room-list-title'>글제목</div>
+                  <div className='room-list-title'>글제목 </div>
                   <div className='room-list-price'>
                     <span className='big-text'>56000</span>
                     <span className='small-text'>원/시간</span>
