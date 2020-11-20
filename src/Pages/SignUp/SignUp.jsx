c7ebed11651acd8855004d57f4cd9fd370d37fc3
dongha <dongha@email.com>
2020-11-20 18:31:30 +0900

    ing

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -3,11 +3,31 @@ import { Link } from "react-router-dom";
 import "./SignUp.scss";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: [],
+      email: "",
+      password: "",
+      rePassword: "",
+      loginPath: {
+        google: false,
+        naver: false,
+        blog: false,
+        recommend: false,
+        brunch: false,
+        app: false,
+        instagram: false,
+        facebook: false,
+        etc: "",
+      },
+    };
+  }
   render() {
     return (
       <section className="SignUp">
         <div className="sign-up-container">
-          <h1>회원가입</h1>
+          <h1> 회원가입 </h1>
           <div className="sign-up-form-wrap">
             <form action="post" className="sign-up-form">
               <div className="authenfication-wrap">
@@ -15,7 +35,7 @@ class SignUp extends Component {
                   htmlFor="authenfication "
                   className="authenfication-title"
                 >
-                  <span> 본인인증</span>
+                  <span> 본인인증 </span>
                 </label>
                 <span className="authenfication-input">
                   <input
@@ -25,66 +45,66 @@ class SignUp extends Component {
                   />
                 </span>
                 <div className="checked-bg">
-                  <i className="fas fa-check checked"></i>
+                  <i className="fas fa-check checked"> </i>
                 </div>
               </div>
               <div className="email-input-wrap">
                 <label htmlFor="email" className="email-input-title">
-                  <span> 이메일 주소</span>
-                </label>
+                  <span> 이메일 주소 </span>{" "}
+                </label>{" "}
                 <span className="email-user-input">
                   <input
                     type="text"
                     id="email"
                     placeholder="이메일 주소를 입력해주세요."
                   />
-                </span>
-              </div>
+                </span>{" "}
+              </div>{" "}
               <div className="password-input-wrap">
                 <label className="password-input-title" htmlFor="password">
-                  <span> 비밀번호</span>
-                </label>
+                  <span> 비밀번호 </span>{" "}
+                </label>{" "}
                 <span className="password-user-input">
                   <input
                     type="text"
                     id="password"
                     placeholder="8-20자 이내 문자의 조합으로 입력해주세요."
                   />
-                </span>
-              </div>
+                </span>{" "}
+              </div>{" "}
               <div className="password-re-input-wrap">
                 <label
                   className="password-re-input-title"
                   htmlFor="re-password"
                 >
-                  <span> 비밀번호 확인</span>
-                </label>
+                  <span> 비밀번호 확인 </span>{" "}
+                </label>{" "}
                 <span className="password-re-user-input">
                   <input
                     type="text"
                     id="re-password"
                     placeholder=" 8-20자 이내 문자의 조합으로 입력해주세요."
                   />
-                </span>
-              </div>
+                </span>{" "}
+              </div>{" "}
               <div className="register-path-wrap">
                 <label className="register-path-title" htmlFor="path">
-                  <div> 가입 경로</div>
-                </label>
+                  <div> 가입 경로 </div>{" "}
+                </label>{" "}
                 <div className="register-path-input-wrap">
                   <ul>
                     <li>
                       <span>
                         <input type="checkbox" value="구글 검색" id="path" />
-                        <span> 구글 검색</span>
-                      </span>
-                    </li>
+                        <span> 구글 검색 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input type="checkbox" value="네이버 검색" id="path" />
-                        <span> 네이버 검색</span>
-                      </span>
-                    </li>
+                        <span> 네이버 검색 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input
@@ -92,9 +112,9 @@ class SignUp extends Component {
                           value="네이버 블로그"
                           id="path"
                         />
-                        <span> 네이버 블로그</span>
-                      </span>
-                    </li>
+                        <span> 네이버 블로그 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input
@@ -102,42 +122,42 @@ class SignUp extends Component {
                           value="지인 추천/소개"
                           id="path"
                         />
-                        <span> 지인 추천/소개</span>
-                      </span>
-                    </li>
+                        <span> 지인 추천 / 소개 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input type="checkbox" value="브런치" id="path" />
-                        <span> 브런치</span>
-                      </span>
-                    </li>
+                        <span> 브런치 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input type="checkbox" value="App 검색" id="path" />
-                        <span> App 검색</span>
-                      </span>
-                    </li>
+                        <span> App 검색 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input type="checkbox" value="인스타그램" id="path" />
-                        <span> 인스타그램</span>
-                      </span>
-                    </li>
+                        <span> 인스타그램 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input type="checkbox" value="페이스북" id="path" />
-                        <span> 페이스북</span>
-                      </span>
-                    </li>
+                        <span> 페이스북 </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
                     <li>
                       <span>
                         <input type="checkbox" value="네이버 검색" id="path" />
-                        <span> 기타(직접 입력)</span>
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+                        <span> 기타(직접 입력) </span>{" "}
+                      </span>{" "}
+                    </li>{" "}
+                  </ul>{" "}
+                </div>{" "}
+              </div>{" "}
               <div className="register-path-self-input-wrap">
                 <span className="register-path-self-input">
                   <input
@@ -145,15 +165,15 @@ class SignUp extends Component {
                     id="register"
                     placeholder="기타 가입 경로를 입력해주세요."
                   />
-                </span>
-              </div>
+                </span>{" "}
+              </div>{" "}
               <div className="recommend-code-input-wrap">
                 <label
                   className="recommend-code-input-title"
                   htmlFor="recommend"
                 >
-                  <span> 추천인 코드(선택)</span>
-                </label>
+                  <span> 추천인 코드(선택) </span>{" "}
+                </label>{" "}
                 <div className="recommend-code-user-input-wrap">
                   <span className="recommend-code-user-input">
                     <input
@@ -161,41 +181,41 @@ class SignUp extends Component {
                       id="recommend"
                       placeholder=" 추천인 코드를 입력해주세요."
                     />
-                  </span>
+                  </span>{" "}
                   <span className="recommend-code-desc">
                     * 추천인 코드를 입력하시면, 첫 촬영 완료시 포인트를
-                    적립해드립니다.
-                  </span>
-                </div>
-              </div>
+                    적립해드립니다.{" "}
+                  </span>{" "}
+                </div>{" "}
+              </div>{" "}
               <div className="terms-agree-wrap">
                 <label className="terms-agree">
                   <div>
                     <input type="checkbox" value="약관 동의" id="term" />
-                    <div> 아워플레이즈 서비스 약관 동의</div>
-                  </div>
+                    <div> 아워플레이즈 서비스 약관 동의 </div>{" "}
+                  </div>{" "}
                   <div>
                     <input
                       type="checkbox"
                       value="개인정보 처리방침"
                       id="term"
                     />
-                    <div> 아워플레이즈 개인정보 처리방침</div>
-                  </div>
-                </label>
-              </div>
-              <div className="divider"></div>
+                    <div> 아워플레이즈 개인정보 처리방침 </div>{" "}
+                  </div>{" "}
+                </label>{" "}
+              </div>{" "}
+              <div className="divider"> </div>{" "}
               <div className="bottom-button-wrap">
                 <input type="button" value="취소하기" />
                 <input type="button" value="회원가입" />
-              </div>
+              </div>{" "}
               <Link to="/" className="back-to-login">
-                <div>로그인으로 돌아가기</div>
-                <i class="fas fa-arrow-right"></i>
-              </Link>
-            </form>
-          </div>
-        </div>
+                <div> 로그인으로 돌아가기 </div>{" "}
+                <i class="fas fa-arrow-right"> </i>{" "}
+              </Link>{" "}
+            </form>{" "}
+          </div>{" "}
+        </div>{" "}
       </section>
     );
   }
