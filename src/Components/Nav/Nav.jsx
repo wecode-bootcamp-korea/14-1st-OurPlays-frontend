6847f1e78dbdb9633c24263f8fb1db934316d70a
dongha <dongha@email.com>
2020-11-23 18:25:14 +0900

    before big enter

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Nav.scss";
 
 class Nav extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
   render() {
     return <div> </div>;
   }
