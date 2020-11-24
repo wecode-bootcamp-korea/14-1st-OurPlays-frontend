dfd7e0d25259d671afb72d6c42749acf78275a9a
jooney <jooneybadger@gmail.com>
2020-11-24 15:41:18 +0900

    fetch추가

--- src/Components/Category/Components/CategoryItems.jsx
+++ src/Components/Category/Components/CategoryItems.jsx
@@ -3,6 +3,29 @@ import { Link } from 'react-router-dom';
 import '../Category.scss';
 
 class CategoryItems extends Component {
+  state = {
+    PLACEINFO: [],
+  };
+
+  // componentDidMount() {
+  //   fetch('/Data/PlaceData.json')
+  //     .then((res) => res.json())
+  //     // .then((res) => {
+  //     //   const datas = res.information;
+  //     //   console.log(datas);
+  //     //   const current = datas.find((el) => el.id == 0);
+  //     //   console.log(current);
+  //     //   this.setState({ PLACEINFO: current });
+  //     // });
+  //     .then((res) => this.setState({ PLACEINFO: res.information }));
+  // }
+
+  componentDidMount() {
+    fetch('http://10.58.3.74:8000/ProductList/ProductDetail')
+      .then((res) => res.json())
+      .then((res) => this.setState({ PLACEINFO: res.information }));
+  }
+
   render() {
     return (
       <div className='category-items'>
