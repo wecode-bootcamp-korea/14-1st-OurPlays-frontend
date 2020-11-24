230bdcd46616d7d1c3911d8fd36d8075e6992546
jooney <jooneybadger@gmail.com>
2020-11-25 00:37:49 +0900

    ing

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -6,19 +6,39 @@ import './Nav.scss';
 class Nav extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { isSticky: false, scrollY: 0 };
+    this.ref = React.createRef();
   }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.scroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scroll);
+  }
+  scroll = () => {
+    if (window.scrollY > 10) {
+      this.setState({
+        isSticky: true,
+      });
+    }
+    if (window.scrollY <= 10) {
+      this.setState({
+        isSticky: false,
+      });
+    }
+  };
 
   render() {
     return (
       <nav>
-        <section className='nav-background'>
+        <section className={this.state.isSticky ? 'nav-background sticky' : 'nav-background'}>
           <section className='main-nav'>
             <div className='logo-icon'>
               <Link to='/'>
                 <img src='https://s3.hourplace.co.kr/web/images/logo_blue.svg' alt='logo-img' />
-              </Link>
-            </div>
+              </Link>{' '}
+            </div>{' '}
             <div className='search-input'>
               <input type='text' placeholder='촬영의 모든 장소' />
               <img
@@ -31,22 +51,22 @@ class Nav extends Component {
                 src='https://s3.hourplace.co.kr/web/images/ico_search.png'
                 alt='search-icon'
               />
-            </div>
+            </div>{' '}
             <NAVICONS Link='Link' />
-          </section>
-        </section>
-        <div className='border-without-side'></div>
-        <section className='side-nav'>
-          <div className='direction'>
-            <Link to='/' className='home'>
-              Home
-            </Link>
-            <img src='../images/youngjoonpark/arrow-img.png' alt='arrow-img' />
-            <Link to='현재경로' className='route'>
-              route
-            </Link>
-          </div>
-        </section>
+          </section>{' '}
+        </section>{' '}
+        <div className='border-without-side'> </div>{' '}
+        {/* <section className="side-nav">
+          <div className="direction">
+            <Link to="/" className="home">
+              Home{" "}
+            </Link>{" "}
+            <img src="../images/youngjoonpark/arrow-img.png" alt="arrow-img" />
+            <Link to="현재경로" className="route">
+              route{" "}
+            </Link>{" "}
+          </div>{" "}
+        </section>{" "} */}
       </nav>
     );
   }
