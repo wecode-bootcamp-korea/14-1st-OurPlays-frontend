936eddd8bb8015c8cb73210ab2bd0f1922d611c0
jooney <jooneybadger@gmail.com>
2020-11-24 12:00:41 +0900

    ing

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CategoryItems from './Components/CategoryItems';
 import './Category.scss';
-import { Link } from 'react-router-dom';
 
 class Category extends Component {
   render() {
