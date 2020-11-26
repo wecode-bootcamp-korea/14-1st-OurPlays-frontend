419980e5335a5198e9e7b9dfbd70dbc6e9a91146
jooney <jooneybadger@gmail.com>
2020-11-26 22:18:02 +0900

    wip

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
-import CategoryItems from "./Components/CategoryItems";
-import "./Category.scss";
 
 class Category extends Component {
   render() {
-    return (
-      <div className="Category">
-        <CategoryItems />
-      </div>
-    );
+    return <div> </div>;
   }
 }
 
