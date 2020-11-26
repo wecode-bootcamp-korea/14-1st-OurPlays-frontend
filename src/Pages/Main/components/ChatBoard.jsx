3bbdc773364294e669fb103f11e515acd25656d8
jooney <jooneybadger@gmail.com>
2020-11-26 09:48:30 +0900

    Fix 에러수정중

--- src/Pages/Main/components/ChatBoard.jsx
+++ src/Pages/Main/components/ChatBoard.jsx
@@ -6,20 +6,19 @@ class ChatBoard extends Component {
 
     return (
       <div className="board-container">
-        {" "}
         {this.props.comments.map((comment) => {
           {
             return (
               <div className="wrap">
-                <div className="user-div"> {comment.comment} </div>{" "}
+                <div className="user-div"> {comment.comment} </div>
                 <div className="chat-div">
-                  <div className="chat-name"> 아워플레이스 </div>{" "}
-                  <div className="chat-content"> {comment.chatComment} </div>{" "}
-                </div>{" "}
+                  <div className="chat-name"> 아워플레이스 </div>
+                  <div className="chat-content"> {comment.chatComment} </div>
+                </div>
               </div>
             );
           }
-        })}{" "}
+        })}
       </div>
     );
   }
