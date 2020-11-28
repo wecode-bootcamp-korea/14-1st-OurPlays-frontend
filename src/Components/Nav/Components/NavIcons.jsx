694d175873d70b7266db6f8ce7a05368355bd9c5
jooney <jooneybadger@gmail.com>
2020-11-29 00:27:54 +0900

    wip

--- src/Components/Nav/Components/NavIcons.jsx
+++ src/Components/Nav/Components/NavIcons.jsx
@@ -10,7 +10,7 @@ class NavIcons extends Component {
       login: false,
     };
   }
-      () {
+  componentDidMount() {
     const isToken = localStorage.setItem("token");
     if (isToken) {
       this.setState({ login: true });
